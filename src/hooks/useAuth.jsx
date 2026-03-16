28ac6d0507953cf73e48283e193dc03312e90405
Robert-Galan <robert.galan@tech4all.ai>
2026-03-16 13:32:40 +0100

    removed automatic storage of username

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -5,8 +5,8 @@ import { apiLogin } from "../api";
 const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(localStorage.getItem(LS_USER) || ""); // DA ELIMINARE
-  const [pass, setPass] = useState(localStorage.getItem(LS_PASS) || ""); // DA ELIMINARE
+//   const [user, setUser] = useState(localStorage.getItem(LS_USER) || ""); // DA ELIMINARE
+//   const [pass, setPass] = useState(localStorage.getItem(LS_PASS) || ""); // DA ELIMINARE
   const [isAuthed, setIsAuthed] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
@@ -16,8 +16,8 @@ export function AuthProvider({ children }) {
     setError("");
     try {
       await apiLogin(user, pass);
-      localStorage.setItem(LS_USER, user.trim()); // DA ELIMINARE
-      localStorage.setItem(LS_PASS, pass);         // DA ELIMINARE
+    //   localStorage.setItem(LS_USER, user.trim()); // DA ELIMINARE
+    //   localStorage.setItem(LS_PASS, pass);         // DA ELIMINARE
       setIsAuthed(true);
     } catch {
       setError("Credenziali non valide o errore di rete");
